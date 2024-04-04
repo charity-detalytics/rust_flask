08f8adc1809de2778d7c270553d6d4b3572ef36e
charity <thien.nguyen@detalytics.com>
2024-04-04 15:50:05 +0700

    test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use pyo3::prelude::*;
-use std::env;
 
 fn main() -> PyResult<()> {
-    env::set_var("PYTHONPATH", "/home/runner/.local/share/virtualenvs/flask_hello-oE1Q3Qlk/bin/python");
-
     let py_app = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/flask_hello/app.py"));
     let flask_run = Python::with_gil(|py| -> PyResult<Py<PyAny>> {
         let app: Py<PyAny> = PyModule::from_code(py, py_app, "", "")?
