f4d9720508549912cc92bd9a67de99e9cbb0d587
charity <thien.nguyen@detalytics.com>
2024-04-04 15:15:29 +0700

    test

--- flask_hello/app.py
+++ flask_hello/app.py
@@ -4,3 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def say_hello():
     return 'Hello, World!'
+
+def run():
+    app.run()
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ fn main() -> PyResult<()> {
     let py_app = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/flask_hello/app.py"));
     let flask_run = Python::with_gil(|py| -> PyResult<Py<PyAny>> {
         let app: Py<PyAny> = PyModule::from_code(py, py_app, "", "")?
-            .getattr("app")?
+            .getattr("run")?
             .into();
-        app.call_method0("run")
+        app.call0(py)
     });
 
     flask_run?;
